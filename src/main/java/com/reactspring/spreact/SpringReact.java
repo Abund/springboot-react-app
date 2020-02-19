4401376b8e66222c6e98b6ecbffd995adeb06623
Abundance Onyeodizielu <abunonyeodi@gmail.com>
2020-02-19 14:36:51 +0100

    Initial commit

---
+++ src/main/java/com/reactspring/spreact/SpringReact.java
@@ -0,0 +1,13 @@
+package com.reactspring.spreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReact {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReact.class, args);
+    }
+
+}
